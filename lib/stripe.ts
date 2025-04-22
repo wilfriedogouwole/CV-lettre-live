b52ec14c5938163a9e4ab2518bea03a0f48430bb
dev2 <dev2@dev2>
2025-04-22 17:15:05 +0200

    integration de stripe'

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,10 +1,6 @@
 import Stripe from "stripe";
 
-if (!process.env.STRIPE_SECRET_KEY) {
-  throw new Error("STRIPE_SECRET_KEY must be defined in your environment variables");
-}
-
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: "2023-10-16",
   typescript: true,
   httpClient: Stripe.createFetchHttpClient(),
